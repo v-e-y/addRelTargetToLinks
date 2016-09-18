2736981c58a9e201066bc4146aeb6ffee7be19e1
Edward V <v-e-y@outlook.com>
2016-09-19 01:11:15 +0300

    Create project.js

---
+++ output/project.js
@@ -0,0 +1,47 @@
+class AddNofollowToLinks {
+    constructor(target, role) {
+        this.pageLinks = document.links;
+        this.pageLinksCount = document.links.length;
+        this.host = window.location.hostname;
+        this.role = !role ? true : role;
+        this.target = !target ? true : target;
+    }
+    addNoFollow() {
+        if (this.pageLinksCount > 1) {
+            for (let i = 0; i < this.pageLinksCount; i++) {
+                let link = this.pageLinks[i];
+                if (this.host != link.hostname) {
+                    link.setAttribute('role', 'nofollow');
+                }
+            }
+        }
+    }
+    addTargetBlank() {
+        if (this.pageLinksCount > 1) {
+            for (let i = 0; i < this.pageLinksCount; i++) {
+                let link = this.pageLinks[i];
+                if (this.host != link.hostname) {
+                    link.setAttribute('target', '_blank');
+                    if (link.attributes.rel.nodeValue = 'nofollow') {
+                        link.setAttribute('role', 'nofollow noopener');
+                    }
+                    else {
+                        link.setAttribute('role', 'noopener');
+                    }
+                }
+            }
+        }
+    }
+    addNoFollowAndTargetBlank() {
+        if (this.pageLinksCount > 1) {
+            for (let i = 0; i < this.pageLinksCount; i++) {
+                let link = this.pageLinks[i];
+                if (this.host != link.hostname) {
+                    link.setAttribute('target', '_blank');
+                    link.setAttribute('role', 'nofollow noopener');
+                }
+            }
+        }
+    }
+}
+//# sourceMappingURL=project.js.map
