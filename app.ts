78d1a3cefef6610b6b3c2885df5ae264293699fc
Edward V <v-e-y@outlook.com>
2016-09-19 08:42:01 +0300

    Add comments

--- app.ts
+++ app.ts
@@ -1,24 +1,30 @@
-
 /**
- * name
+ * Edward
+ * v-e-y@outlook.com
+ * free use
  */
 class AddNofollowToLinks {
     pageLinks: HTMLCollection;
     pageLinksCount: number;
-    role: boolean;
-    target: boolean;
+    // role: boolean;
+    // target: boolean;
     host: string;
 
     // pageLinks: HTMLCollection, host: string, pageLinksCount: number, 
-    constructor(target: boolean, role: boolean) {
+    // target: boolean, role: boolean
+    constructor() {
+        // All links in page
         this.pageLinks = document.links;
+        // Count links in page
         this.pageLinksCount = document.links.length;
+        // Hostname in page
         this.host = window.location.hostname;
-        this.role = !role ? true : role;
-        this.target = !target ? true : target;
-        
+
+        // this.role = !role ? true : role;
+        // this.target = !target ? true : target;
     }
 
+    // Add 'role="nofollow"' to link, exemple: <a href="http://www.exemple.com/some-url" title="Some page" rel="nofollow">some link</a>
     addNoFollow() {
         if (this.pageLinksCount > 1) {
             for (let i = 0; i < this.pageLinksCount; i++) {
@@ -31,16 +37,22 @@ class AddNofollowToLinks {
         }
     }
 
+    // Add 'target="_blank"' to link, exemple: <a href="http://www.exemple.com/some-url" title="Some page" target="_blank">some link</a>
+    // And add noopener, why, you can read here - https://news.ycombinator.com/item?id=11553740
     addTargetBlank() {
         if (this.pageLinksCount > 1) {
             for (let i = 0; i < this.pageLinksCount; i++) {
                 let link: Element = this.pageLinks[i];
+                // if curent host != link host
                 if (this.host != link.hostname) {
                     link.setAttribute('target', '_blank');
                     // додати перевірку на інші можливі значення
+                    // if link have nofollow
                     if (link.attributes.rel && link.attributes.rel.nodeValue == 'nofollow') {
+                        // Add noopener
                         link.setAttribute('role', 'nofollow noopener');
                     } else {
+                        // Else add only noopener
                         link.setAttribute('role', 'noopener');
                     }
                     
