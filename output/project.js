d620bf5e6f038731d11a041b18d3947975a53528
Edward V <v-e-y@outlook.com>
2016-09-19 08:43:39 +0300

    Update project.js

--- output/project.js
+++ output/project.js
@@ -1,10 +1,8 @@
 class AddNofollowToLinks {
-    constructor(target, role) {
+    constructor() {
         this.pageLinks = document.links;
         this.pageLinksCount = document.links.length;
         this.host = window.location.hostname;
-        this.role = !role ? true : role;
-        this.target = !target ? true : target;
     }
     addNoFollow() {
         if (this.pageLinksCount > 1) {
@@ -22,8 +20,7 @@ class AddNofollowToLinks {
                 let link = this.pageLinks[i];
                 if (this.host != link.hostname) {
                     link.setAttribute('target', '_blank');
-                    // додати перевірку на інші можливі значення
-                    if (link.attributes.rel.nodeValue == 'nofollow') {
+                    if (link.attributes.rel && link.attributes.rel.nodeValue == 'nofollow') {
                         link.setAttribute('role', 'nofollow noopener');
                     }
                     else {
@@ -45,4 +42,3 @@ class AddNofollowToLinks {
         }
     }
 }
-//# sourceMappingURL=project.js.map
